b062f6d0b3be8d73a38cd6c64b344496471ebaa8
jdeng <jdeng@jdeng2.Smart-Trade.net>
2017-02-16 18:09:04 +0100

    CountDownLatch

---
+++ src/main/java/com/chapitre10/item69/CountDownLatchTest.java
@@ -0,0 +1,13 @@
+package com.chapitre10.item69;
+
+
+//Main thread start
+//Create CountDownLatch for N threads
+//Create and start N threads
+//Main thread wait on latch
+//N threads completes there tasks are returns
+//Main thread resume execution
+
+public class CountDownLatchTest {
+
+}
