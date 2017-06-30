74902c3c1c06769ba7c035ad9b9814e28f38b6f6
jdeng <jdeng@jdeng2.Smart-Trade.net>
2017-06-30 09:53:18 +0200

    interview

--- src/main/java/com/junhua/interview/MainTest.java
+++ src/main/java/com/junhua/interview/MainTest.java
@@ -18,6 +18,17 @@ public class MainTest {
 		test7();
 		
 		test8();
+		
+		test9();
+
+		test10();
+	
+		test11();
+		
+		test12();
+
+		test13();
+		
 	}
 
 	public static boolean find(int[][] array, int value) {
@@ -347,4 +358,131 @@ public class MainTest {
 	public static void test8(){
 		 printOneToNthDigits(2);  
 	}
+	
+	public static void test9(){
+		int[] array = {5,1,12,-5,16,2,12,14};
+		selectionSort(array);
+
+		for(int i : array){
+			System.out.println(i);
+		}
+	}
+	public static void test10(){
+		int[] array = {5,1,12,-5,16,2,12,14};
+		selectionSort2(array);
+		
+		for(int i : array){
+			System.out.println(i);
+		}
+	}
+	
+	public static void selectionSort(int[]array){
+		int size = array.length;
+		int temp = 0;
+		for(int i=0;i<size-1;i++){
+			for(int j=i+1;j<=size-1;j++){
+				if(array[i]>array[j]){
+					temp = array[i];
+					array[i] =array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void selectionSort2(int[]array){
+		int size = array.length;
+		int temp = 0;
+		for(int i=0;i<size-1;i++){
+			for(int j=i+1;j<=size-1;j++){
+				if(array[i]<array[j]){
+					temp = array[i];
+					array[i] =array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void insertionSort(int[]array){
+		int size = array.length;
+		int temp = 0;
+		
+		for(int i=1;i<size;i++){
+			for(int j=i; j>0;j--){
+				if(array[j]<array[j-1]){
+					temp = array[j];
+					array[j] = array[j-1];
+					array[j-1] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void test11(){
+		int[] array = {5,1,12,-5,16,2,12,14};
+		insertionSort(array);
+		
+		for(int i : array){
+			System.out.println(i);
+		}
+	}
+	
+	public static void bubbleSort(int[]array){
+		int size = array.length;
+		int temp = 0;
+		for(int i = 0;i<size-1;i++){
+			
+		}
+	}
+	
+	public static void quickSort(int[]array, int start, int end){
+	
+		int i = start;
+		int j = end;
+		int temp = 0;
+		
+		int pivot = array[start+(end-start)/2];
+		while(i<=j)  {
+			while(pivot>array[i])i++;
+			while(pivot<array[j])j--;
+			if(i<=j){
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		if(start<j)quickSort(array, start, j);
+		if(i<end)quickSort(array, i, end);
+	}
+	
+	
+	public static void test12(){
+		int[] array =  {6, 5, 3, 1, 8, 7, 2, 4};
+		quickSort(array, 0, array.length-1);
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
+	
+	public static void doMergeSort(int lowerIndex, int higherIndex){
+		
+		if(lowerIndex < higherIndex){
+			int mid = lowerIndex + (higherIndex-lowerIndex)/2;
+			System.out.print(mid);
+			doMergeSort(lowerIndex, mid);
+			doMergeSort(mid+1, higherIndex);
+		}
+	}
+	
+	public static void test13(){
+		System.out.println("test13");
+		int[] array =  {6, 5, 3, 1, 8, 7, 2, 4};
+		doMergeSort(0, array.length-1);
+	}
+	
 }
